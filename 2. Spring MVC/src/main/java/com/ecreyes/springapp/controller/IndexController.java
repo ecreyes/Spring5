51f072252b2c46fff7fb02726b7139106562a9d3
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2019-10-04 12:25:53 -0300

    creación de rutas de primer nivel

--- 2. Spring MVC/src/main/java/com/ecreyes/springapp/controller/IndexController.java
+++ 2. Spring MVC/src/main/java/com/ecreyes/springapp/controller/IndexController.java
@@ -3,10 +3,14 @@ package com.ecreyes.springapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-/** Creación de la clase controller para index, cada método de esta clase
- *  se encarga de procesar una petición distinta **/
+/** - Creación de la clase controller para index, cada método de esta clase
+ *    se encarga de procesar una petición distinta
+ *  - El RequestMapping creara la ruta de primer nivel, es decir ahora se
+ *    podra acceder desde /app/index**/
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
     /** - El GetMapping es la ruta que va a procesar el método del tipo GET.
