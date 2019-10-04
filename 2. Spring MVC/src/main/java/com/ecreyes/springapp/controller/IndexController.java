7786296d06d7b3d5644296b8caea292e6ba9a1d4
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2019-10-04 12:18:50 -0300

    pasar parametros del controlador a la vista

--- 2. Spring MVC/src/main/java/com/ecreyes/springapp/controller/IndexController.java
+++ 2. Spring MVC/src/main/java/com/ecreyes/springapp/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.ecreyes.springapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /** Creación de la clase controller para index, cada método de esta clase
@@ -8,11 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    /** El GetMapping es la ruta que va a procesar el método del tipo GET.
-     * Como este método retorna index debe retornar una plantilla
-     * que se llame index, ubicada en resources/templates **/
+    /** - El GetMapping es la ruta que va a procesar el método del tipo GET.
+     * - Como este método retorna index debe retornar una plantilla
+     *   que se llame index, ubicada en resources/templates.
+     * - El Model sirve para enviar parámetros la vista. **/
     @GetMapping(value="/index")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("titulo","Inicio");
+        model.addAttribute("welcome","Bienvenido al sitio");
         return "index";
     }
 }
