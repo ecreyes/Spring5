c3715badf084ccaf46b5babe7a1acd5645bf2e86
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2019-10-18 10:02:03 -0300

    primary y qualifier en decoradores<2

--- 3. Spring DI/src/main/java/com/ecreyes/springapp/controller/IndexController.java
+++ 3. Spring DI/src/main/java/com/ecreyes/springapp/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.ecreyes.springapp.controller;
 
 import com.ecreyes.springapp.model.service.IService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @Autowired
+    @Qualifier("MiServicioComplejo")
     private IService service;
 
     @GetMapping("/")
--- 3. Spring DI/src/main/java/com/ecreyes/springapp/model/service/MiServicioComplejo.java
+++ 3. Spring DI/src/main/java/com/ecreyes/springapp/model/service/MiServicioComplejo.java
@@ -3,11 +3,10 @@ package com.ecreyes.springapp.model.service;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
-@Primary
-@Service("MiServicio")
-public class MiServicio implements IService {
+@Service("MiServicioComplejo")
+public class MiServicioComplejo implements IService {
     @Override
     public String operacion() {
-        return "operación muy costosa....";
+        return "esta es otro tipo de operación compleja";
     }
 }
