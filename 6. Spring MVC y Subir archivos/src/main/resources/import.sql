4c5422e85612380eb11e730848b8506025d4b29f
ecreyes <eduardo.reyeso.14@sansano.usm.cl>
2019-11-03 19:12:26 -0300

    actualizacion del modelo y configuraciones para subir archivos en properties

--- 6. Spring MVC y Subir archivos/src/main/resources/import.sql
+++ 6. Spring MVC y Subir archivos/src/main/resources/import.sql
@@ -1,14 +1,14 @@
 /* poblar tabla */
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (1,'Eduardo','Reyes','edu@gmail.com','2019-10-18');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (2,'Carlos','Reyes','carl@gmail.com','2019-10-18');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (3,'Ignacio','Reyes','ig@gmail.com','2019-10-18');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (4,'nombre4','apellido4','cliente4g@gmail.com','2019-11-03');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (5,'nombre5','apellido5','cliente5g@gmail.com','2019-11-03');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (6,'nombre6','apellido6','cliente6g@gmail.com','2019-11-03');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (7,'nombre7','apellido7','cliente7g@gmail.com','2019-11-03');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (8,'nombre8','apellido8','cliente8g@gmail.com','2019-11-03');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (9,'nombre9','apellido9','cliente9g@gmail.com','2019-11-03');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (10,'nombre10','apellido10','cliente10g@gmail.com','2019-11-03');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (11,'nombre11','apellido11','cliente11g@gmail.com','2019-11-03');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (12,'nombre12','apellido12','cliente12g@gmail.com','2019-11-03');
-INSERT INTO clientes(id,nombre,apellido,email,create_at) VALUES (13,'nombre13','apellido13','cliente13g@gmail.com','2019-11-03');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (1,'Eduardo','Reyes','edu@gmail.com','2019-10-18','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (2,'Carlos','Reyes','carl@gmail.com','2019-10-18','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (3,'Ignacio','Reyes','ig@gmail.com','2019-10-18','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (4,'nombre4','apellido4','cliente4g@gmail.com','2019-11-03','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (5,'nombre5','apellido5','cliente5g@gmail.com','2019-11-03','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (6,'nombre6','apellido6','cliente6g@gmail.com','2019-11-03','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (7,'nombre7','apellido7','cliente7g@gmail.com','2019-11-03','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (8,'nombre8','apellido8','cliente8g@gmail.com','2019-11-03','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (9,'nombre9','apellido9','cliente9g@gmail.com','2019-11-03','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (10,'nombre10','apellido10','cliente10g@gmail.com','2019-11-03','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (11,'nombre11','apellido11','cliente11g@gmail.com','2019-11-03','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (12,'nombre12','apellido12','cliente12g@gmail.com','2019-11-03','');
+INSERT INTO clientes(id,nombre,apellido,email,create_at,foto) VALUES (13,'nombre13','apellido13','cliente13g@gmail.com','2019-11-03','');
